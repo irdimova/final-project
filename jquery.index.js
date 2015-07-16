2cb213a02a56817c409387ea5d0d94d87645286e
irdimova <irdimova@gmail.com>
2015-07-16 19:23:31 +0100

    Initial commit

---
+++ jquery.index.js
@@ -0,0 +1,39 @@
+(function($) {  
+  
+   // find the position of the first image  
+   var firstImage = $('#slider-content .slider-img:first').index();  
+   // find the position of the last image  
+   var lastImage = $('#slider-content .slider-img:last').index();  
+  
+   // set current, next and previous image  
+   var currentImage = firstImage  
+   var nextImage = firstImage + 1  
+   var prevImage = lastImage  
+  
+   var sliderImages = $('#slider-content .slider-img');  
+   var sliderContent = $('#slider-content');  
+  
+   // find the image width    
+   var sliderImageWidth = parseFloat(sliderImages.eq(0).css('width'));  
+  
+   // when clicking the next button find out the next image position (nextImage)  
+   // if currentImage == lastImage - your next image (nextImage) will grab the position of the firstImage  
+   // otherwise nextImage = currentImage + 1  
+  
+   // calculate how much sliderContent will slide   
+   // use animate function to slide  
+   // set nextImage to be current image   
+   $('#button-next').click(function() {  
+       nextImage = currentImage == lastImage ? firstImage : currentImage + 1;  
+       sliderContent.animate({ "left": -nextImage * sliderImageWidth });  
+       currentImage = nextImage;  
+       e.preventDefault();    
+   });  
+   $('#button-prev').click(function() {  
+       prevImage = currentImage == firstImage ? lastImage : currentImage - 1;  
+       sliderContent.animate({ "left": -prevImage * sliderImageWidth });  
+       currentImage = prevImage;  
+       e.preventDefault();    
+   });  
+})(jQuery);  
+
